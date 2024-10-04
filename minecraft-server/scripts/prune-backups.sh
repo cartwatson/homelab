c79ea3cec3ba534cf4b2a4fb16b20d03e81454e5
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2024-10-03 17:17:48 -0700

    add: archive a backup every week; fix: deletion so it doesn't delete archives

--- minecraft-server/scripts/prune-backups.sh
+++ minecraft-server/scripts/prune-backups.sh
@@ -1,10 +1,13 @@
-#!/bin/usr/env bash
+#!/usr/bin/env bash
 # script to prune backups of minecraft servers
 
 backup_dir="/home/cwatson/minecraft/backups"
 
 worlds=("nexus")
 for world in "${worlds[@]}"; do
-  # prune backups older than 30 days
-  find "$backup_dir" -type f -mtime +30 -exec rm {} \;
+  # "archive" backup from seven days ago
+  find "$backup_dir/$world" -maxdepth 1 -type f -mtime 7 -exec mv {} "$backup_dir/$world/archive/" \;
+
+  # prune backups older than 7 days
+  find "$backup_dir/$world" -maxdepth 1 -type f -mtime +7 -exec rm {} \;
 done
